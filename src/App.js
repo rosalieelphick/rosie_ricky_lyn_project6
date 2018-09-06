fd56cfea30b0cb6336a5fcab34fac9465118162b
rosalieelphick <rosalie.elphick@gmail.com>
2018-09-06 17:13:18 -0400

    username and avatars added

--- src/App.js
+++ src/App.js
@@ -23,7 +23,8 @@ class App extends Component {
       difficulty: "easy",
       category: "23", 
       numberOfPlayers: "",
-      promise:{}
+      promise:{},
+      playerArray: []
     }
   }
 
@@ -61,24 +62,45 @@ class App extends Component {
     })
   }
 
+  addPlayers = (playerArray) => {
+    this.setState({
+      playerArray
+    }, () => {
+      playerArray.forEach((player) => {
+        const playerName = player.username
+        this.setState({
+          [playerName]:{score:0}
+        })
+      })
+    })
+  }
+
   render() {
     return (
       <Router>
+
         <div className="App">
           <Route exact path="/" component={LandingPage}/>
 
           <Route exact path="/start" render={(props) => <StartingPage {...props}
           submitPlayers={this.submitPlayers} />}/>
-          <Route exact path="/players" component={Players}/ >
+
+          <Route exact path="/players" render={(props) => <Players {...props} 
+          numberOfPlayers={this.state.numberOfPlayers}
+          addPlayers={this.addPlayers} />} />
+            
           <Route exact path="/choice" render={(props) =>
               <Choice {...props} getQuestions={this.getQuestions} /> 
             } />
+
           <Route 
             exact path="/questions" 
             render={(props) =>
               <Questions {...props} questions={this.state.questions}/> 
             } />
+
           <Route path="/results" component={Results} />
+
         </div>
       </Router>
     );
